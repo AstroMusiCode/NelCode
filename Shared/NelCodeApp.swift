535fdeb337d0af82e82002acc2ab1788b1a82116
Arnav Nelli <arnavnelli@Arnavs-MacBook-Pro.local>
2022-08-13 10:00:47 +0530

    initial xcode project creation

---
+++ Shared/NelCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NelCodeApp.swift
+//  Shared
+//
+//  Created by Arnav Nelli on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct NelCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
